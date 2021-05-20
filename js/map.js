467250f3b48b2a208e5edd8d46926f4d46451174
zodwieg <s.k.kondratiev@gmail.com>
2021-05-20 14:59:41 +0300

    initial commit

---
+++ js/map.js
@@ -0,0 +1,25 @@
+const map = L.map("mapid").setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoiem9kd2llZyIsImEiOiJja293cm01bzcwOHhmMm9vYWJ6bHFpZ21rIn0.tYJCMs2oOcAcWji2tOJVdg'
+}).addTo(map);
+
+const marker = L.marker([51.5, -0.09]).addTo(map);
+
+const circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+const polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
